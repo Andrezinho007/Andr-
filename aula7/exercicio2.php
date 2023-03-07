689d3525eccd133c0e23826a6d855e597b5923f6
AndrÃ© Felipe Mota da Silva <13707594602@dc01.ifs.ifsuldeminas.edu.br>
2023-03-07 13:15:33 -0300

    Exercicio Vetores aula 8

---
+++ aula7/exercicio2.php
@@ -0,0 +1,10 @@
+<?php
+$nome = [];
+$nome[] = "Murilo";
+$nome[] = "Poliana";
+$nome[] = "Dalva";
+$nome[] = "Lavinia";
+$nome[] = "Kaua";
+for($contador = 0; $contador <5  ; $contador++)
+        echo "<li> ".$nome[$contador]."</li>";
+?>
