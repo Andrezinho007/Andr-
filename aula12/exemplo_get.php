4af8285b48d53454f2e443cfe0622b7704258fba
AndrÃ© Felipe Mota da Silva <13707594602@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-20 08:42:27 -0300

    aula exemplo get

---
+++ aula12/exemplo_get.php
@@ -0,0 +1,6 @@
+<?php
+$resultado = $_GET ['numero1'] + $_GET ['numero2'];
+echo "O resultado da soma é $resultado";
+
+// Esse código é o resultado da soma de dois números, o resultado será infomado atravez da URL 
+?>
---
+++ aula12/exercicio.php
@@ -0,0 +1,12 @@
+<h2>Exercicio</h2>
+<?php
+
+$numero = $_GET['numero'];
+
+// Loop para exibir a tabuada
+for($i = 1; $i <= 10; $i++){
+    $resultado = $numero * $i;
+    echo "$numero x $i = $resultado <br>";
+}
+
+?>
